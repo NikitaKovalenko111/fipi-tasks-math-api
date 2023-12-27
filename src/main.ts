bc508393acfe7786f42914c7eadd5f6b08d25dce
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2023-12-27 19:45:52 +0500

    12/27/2023

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
     app.enableCors({
-        origin: 'http://localhost:3001',
+        origin: 'https://fipimath.netlify.app',
         credentials: true,
     })
     app.use(cookieParser())
